84510a5ca4680afc0405ee39047c0eae5d6ec4bb
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-12-02 16:00:17 +0530

    Create client_base.py

---
+++ temp/client_base.py
@@ -0,0 +1,19 @@
+from konfig import API_HASH, API_ID, BOT_TOKEN, STRING_SESSION
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "core"},
+)
+
+user = Client(
+    STRING_SESSION,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
+
+call_py = PyTgCalls(user, overload_quiet_mode=True)
