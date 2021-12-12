3f9f3c87a719ea24f5a8f23a113e73d4a8922614
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-12-12 19:23:30 +0530

    Update stream.py

--- core/stream.py
+++ core/stream.py
@@ -44,7 +44,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["play", f"play@{BOT_USERNAME}"]) & other_filters)
+@b.on_message(filters.command("play") & ~filters.edited)
 async def kkplay(c: Client, m: Message):
   replied = m.reply_to_message
   chat_id = m.chat.id
